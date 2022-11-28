64e65bd47f93f4b91d02387166027b1189a4054d
Ukson <jankowskilukasz1337@gmail.com>
2022-11-28 22:45:49 +0100

    VTP-18: Initial commit

---
+++ web/src/main/java/voteplus/VotePlusDownloader/VotePlusDownloaderApplication.java
@@ -0,0 +1,13 @@
+package voteplus.VotePlusDownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotePlusDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotePlusDownloaderApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/voteplus/VotePlusDownloader/VotePlusDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package voteplus.VotePlusDownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotePlusDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
